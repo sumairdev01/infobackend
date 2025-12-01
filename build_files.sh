d7cdefac5d3e048d2a77077a6cc7ade204032c65
Sumair Ali <flavorsoflife123@gmail.com>
2025-12-02 00:06:50 +0500

    Ensure staticfiles_build creation and add static dir

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,9 @@
+set -e
 echo "BUILD START"
 python3.9 -m ensurepip
 python3.9 -m pip install -r requirements.txt
+mkdir -p staticfiles_build
 python3.9 backend/manage.py collectstatic --noinput --clear
+echo "Listing staticfiles_build content:"
+ls -R staticfiles_build
 echo "BUILD END"
